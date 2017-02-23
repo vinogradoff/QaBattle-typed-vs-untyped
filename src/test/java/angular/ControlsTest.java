13302827393440b3e3d17845bd10918d62074cad
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-23 13:39:30 +0100

    added test for controls

---
+++ src/test/java/angular/ControlsTest.java
@@ -0,0 +1,32 @@
+package angular;
+
+import com.codeborne.selenide.*;
+import org.junit.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * @author Alexei Vinogradov
+ */
+public class ControlsTest {
+
+  @BeforeClass
+  public static void openSite() {
+    open("http://localhost:8080/demo/input");
+  }
+
+  @Test
+  public void checkboxTest() {
+    SelenideElement acceptTerms = $("md-checkbox");
+    acceptTerms.click();
+    acceptTerms.shouldHave(attribute("area-checked", "true"));
+  }
+
+  @Test
+  public void switchTest() {
+    SelenideElement enableSpecialOptions = $("md-switch");
+    enableSpecialOptions.click();
+    enableSpecialOptions.shouldHave(attribute("area-checked", "true"));
+  }
+}
